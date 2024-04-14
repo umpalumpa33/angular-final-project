fe235b6ccbb5f448f03012e3f3e8b8ccb4d1af3f
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2024-04-14 17:56:29 +0400

    cota fixebi

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../interfaces/user.interface';
-import { Posts } from '../interfaces/posts.interface'
+import { Posts } from '../interfaces/posts.interface';
 import { ApiService } from '../api.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-
-
-
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-posts',
@@ -16,20 +12,20 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
-
   posts: Posts[] = [];
   showForm = false;
   addForm!: FormGroup;
-  comments$: Observable<Comment[]> | undefined;
   usersMap: { [key: number]: string } = {};
   newAuthor!: string;
   newBody!: string;
   newTitle!: string;
-  newUserName: any;
   dataReady = false;
 
-  constructor(private apiService: ApiService, private fb: FormBuilder, private router: Router, private http: HttpClient) { }
- 
+  constructor(
+    private apiService: ApiService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.addForm = this.fb.group({
@@ -38,71 +34,50 @@ export class PostsComponent implements OnInit {
       body: ['', Validators.required]
     });
 
+    forkJoin([
+      this.apiService.getUsers(),
+      this.apiService.getPosts()
+    ]).subscribe(([users, posts]) => {
+      for (const user of users) {
+        this.usersMap[user.id] = user.name;
+      }
 
-    this.apiService.getUsers().subscribe((users: User[]) => {
-      setTimeout(() => {
-        for (const user of users) {
-          this.usersMap[user.id] = user.name;
-        }
-      });
-    });
+      this.posts = posts.map(post => ({
+        ...post,
+        userName: this.usersMap[post.userId]
+      }));
 
-    this.apiService.getPosts().subscribe((posts: Posts[]) => {
-      setTimeout(() => {
-        this.posts = posts.map(post => {
-          return {
-            ...post,
-            userName: this.usersMap[post.userId]
-          };
-        });
-      });
+      this.dataReady = true;
     });
   }
-
 
   navigateToPost(post: Posts): void {
     this.router.navigate(['/posts', post.id]);
   }
+
   addPost() {
     if (this.addForm.valid) {
-      let newUserId = 1; 
-      let newId = 1; 
-      
-      if (this.posts.length > 0) {
-        newUserId = this.getLastUserId() + 1; 
-        newId = this.posts[this.posts.length - 1].id + 1; 
-      }
-      
-      const newPost = {
-        userId: newUserId,
-        id: newId,
-        title: this.newTitle,
-        body: this.newBody,
-        userName: this.newAuthor
-      };
-  
-      this.posts.unshift(newPost); 
-  
+      const newUserId = this.posts.length > 0 ? this.getLastUserId() + 1 : 1;
+      const newId = this.posts.length > 0 ? this.posts[this.posts.length - 1].id + 1 : 1;
+      const newPost = { userId: newUserId, id: newId, title: this.newTitle, body: this.newBody, userName: this.newAuthor };
+      this.posts.unshift(newPost);
       this.newTitle = '';
       this.newBody = '';
       this.newAuthor = '';
-  
     } else {
       console.log('Form is invalid');
     }
   }
-  
 
- getFormControl(name: string) {
+  getFormControl(name: string) {
     return this.addForm.get(name);
   }
+
   getLastUserId(): number {
     return this.posts[this.posts.length - 1].userId;
   }
 
   toggleForm() {
     this.showForm = !this.showForm;
-    console.log()
   }
-
 }
